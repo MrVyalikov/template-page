06343d334ed15564148867b9d3e5991c9672f3a6
The Dog Rex <vyalikov@mail.ru>
2020-03-05 00:01:31 +0300

    small codestyle fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,28 @@
 import './vendor';
 
-var colors = [ "#ffc815", "#a3cd3b", "#0093d7"];
-var maxCounter = 1200;
+let colors = ['#ffc815', '#a3cd3b', '#0093d7'];
+let maxCounter = 1200;
 
 // get color index from counter value
-function getColorIndexFromValue(counter)
-{
-	var ind = Math.floor(counter*3/maxCounter);
-	if(ind < 0) ind = 0;
-	if(ind > 2) ind = 2;
+function getColorIndexFromValue(counter) {
+	let ind = Math.floor(counter*3/maxCounter);
+
+	if (ind < 0) ind = 0;
+	if (ind > 2) ind = 2;
+
 	return ind;
 }
 
 // updates counter and rotates an arrow
-function updateCounter(value, arrow, text)
-{
-	text.text(Math.round(value));
+function updateCounter(value, arrow, text) {
 	var colorIndex = getColorIndexFromValue(value);
+
+	text.text(Math.round(value));
 	text.css({color: colors[colorIndex]});
-	if(arrow)
-	{
-		var angle = 180*(value/maxCounter);
+
+	if(arrow) {
+		let angle = 180*(value/maxCounter);
+
 		arrow.css({
 			transform: 'rotate(' + angle + 'deg)'
 		});
@@ -29,20 +31,21 @@ function updateCounter(value, arrow, text)
 
 // start counter animation when page loaded
 $(document).ready(function() {
-	$(".counter-animated").each(function () {
-		var $this = $(this);
-		var arrow = $("#arrow");
-		var countTo =  $this.attr("countTo");
-		$this.text("0");
+	$('.counter-animated').each( () => {
+		let counter = $(this);
+		let arrow = $('#arrow');
+		let countTo =  counter.attr('countTo');
+
+		counter.text("0");
 		jQuery({Counter: 0}).animate({Counter: countTo}, {
 			duration: 2500,
 			delay: 3000,
 			easing:  'swing',
-			step: function () {
-				updateCounter(this.Counter, arrow, $this)
+			step: () => {
+				updateCounter(this.Counter, arrow, counter)
 			},
-			complete: function() {
-				updateCounter(countTo, arrow, $this)
+			complete: () => {
+				updateCounter(countTo, arrow, counter)
 			}
 		});
 	});
